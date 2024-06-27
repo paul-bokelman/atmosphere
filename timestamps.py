31aee8beed455ce16f0252420f138e628505c86a
Paul Bokelman <paul.bokelman1@gmail.com>
2024-06-27 14:35:37 -0700

    task: audio timestamps and keywords (#2)

--- timestamps.py
+++ timestamps.py
@@ -4,19 +4,24 @@ import google.generativeai as genai
 
 load_dotenv()
 
+# configuration
 API_KEY = os.getenv('GEMINI_API_KEY')
-
 genai.configure(api_key=API_KEY)
 
-instructions = "Your job is to receive an audio book sample and understand the setting. Find 5-10 timestamps throughout the audio to place ambient sound effects like 'leaves rustling' or 'water flowing'. Example response: 0:15-0:32 - leaves rustling in the wind."
+# system instructions passed into model
+instructions = """
+Your job is to receive an audio book sample then find spots where ambient sound effects could be added. For each spot, return it's timestamp interval and a few keywords to describe the desired ambient sound effect. Your response should follow this schema: [{"interval": str, "keywords": str[]}, ...]
+"""
 
-model = genai.GenerativeModel(model_name='gemini-1.5-flash', system_instruction=instructions)
+# initialize model with system instructions and json response
+model = genai.GenerativeModel(model_name='gemini-1.5-flash', 
+                              system_instruction=instructions, 
+                              generation_config={"response_mime_type": "application/json"})
 
-aud = genai.get_file(name="files/8l3chfazgyw3")
+aud = genai.get_file(name="files/8l3chfazgyw3") # get uploaded file from google servers
+response = model.generate_content([aud]) # generate timestamps and keywords in JSON format
 
-prompt = "Give short responses of some timestamps and what general sound effect might be heard. For example: 0:15-0:32 - leaves rustling in the wind."
-
-prompt2 = "Give a general ambient sound for the overall setting of area."
-
-response = model.generate_content([aud])
-print(response.text)
+# output response to json file
+f = open("out/woz1-timestamps.json", "w")
+f.write(response.text)
+f.close()
